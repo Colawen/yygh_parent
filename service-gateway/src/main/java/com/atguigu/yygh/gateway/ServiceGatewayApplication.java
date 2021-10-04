57d02942baa12231a8bb2d1e8dd04f7a6c971c8b
Colawen <dzw96@outlook.com>
2021-10-04 22:33:27 +0800

    预约挂号系统管理员后台完成

---
+++ service-gateway/src/main/java/com/atguigu/yygh/gateway/ServiceGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.yygh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dzwstart
+ * @date 2021/10/4 - 13:51
+ */
+@SpringBootApplication
+public class ServiceGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class,args);
+    }
+}
